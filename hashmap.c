aed414772ac04b08625a97094eb4146b34e66b27
CRISTOBAL ANDRES RUBILAR VICENCIO <134748339+cristobalrubilar@users.noreply.github.com>
2024-04-24 18:09:22 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -100,6 +100,7 @@ Pair * searchMap(HashMap * map,  char * key)
     if(is_equal(map->buckets[posicion]->key,key))
     {
       return map->buckets[posicion]; 
+      map->current = posicion;
     }
   }
 
