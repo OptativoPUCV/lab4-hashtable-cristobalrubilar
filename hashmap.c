e604e1229a5adb7d65cf8bdedf31b711c6071bf1
CRISTOBAL ANDRES RUBILAR VICENCIO <134748339+cristobalrubilar@users.noreply.github.com>
2024-04-24 14:48:00 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -42,7 +42,7 @@ int is_equal(void* key1, void* key2){
 void insertMap(HashMap * map, char * key, void * value) 
 {
   int posicion = hash(key,map->capacity);
-  if (map->buckets[posicion] == NULL || map->buckets[posicion]->key == NULL)
+  if (map->buckets[posicion] == NULL)
   {
     map->buckets[posicion] = createPair(key, value);
     (map->size)++;
